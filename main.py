b10e49a5c6a7ac87762cbca567e08f1a8d28a367
mageshyt <mageshytdev@gmail.com>
2026-02-26 20:16:25 +0530

    [Added] implement main function to utilize LLMProvider for message handling

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from ai-agent-python!")
+from provider.llm_provider import LLMProvider
+from asyncio import run
 
+async def main():
+    llm = LLMProvider()
+    message = [
+        {"role": "user", "content": "What is the capital of France?"}
+    ]
+
+    async for event in  llm.send_message(message, stream=True):
+        print(event)
+
+    print("Done")
 
 if __name__ == "__main__":
-    main()
+    run(main())
+
