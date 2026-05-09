157d4c68c3bebc9c891e8408e8c68b51ba4ef4c1
mageshyt <mageshytdev@gmail.com>
2026-05-09 22:11:43 +0530

    fix: update import statements and include _hash_args in __all__ exports

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,5 +1,5 @@
-from lib.paths import MAX_FILE_SIZE, check_file_size, is_binary_file, resolve_path,get_relative_path,ensure_parent_directory
-from lib.text import count_tokens, truncate_text_by_tokens
+from .paths import MAX_FILE_SIZE, check_file_size, is_binary_file, resolve_path,get_relative_path,ensure_parent_directory
+from .text import count_tokens, truncate_text_by_tokens , _hash_args
 from .contants.config import CONFIG_FILE_NAME , AGENT_MD_FILE_NAME, MAX_FILE , IGNORED_DIRECTORIES, BLOCKED_COMMANDS, BLOCKED_FILES, APP_NAME
 from .errors import AgentError, ConfigError 
 from .safety import is_safe_command , is_dangerous_command
@@ -9,5 +9,5 @@ __all__ = [
         "MAX_FILE_SIZE", "check_file_size", "is_binary_file", "resolve_path", "count_tokens", "truncate_text_by_tokens","get_relative_path", 
         "CONFIG_FILE_NAME", "AgentError", "ConfigError", "AGENT_MD_FILE_NAME","ensure_parent_directory", "MAX_FILE",
         "IGNORED_DIRECTORIES", "BLOCKED_COMMANDS", "BLOCKED_FILES", "APP_NAME",
-        "is_dangerous_command" , "is_safe_command"
+        "is_dangerous_command" , "is_safe_command", "_hash_args"
         ]
